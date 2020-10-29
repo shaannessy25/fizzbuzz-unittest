d0186e93ff946b61527e467692e3dbe3a6ea67e5
shaannessy25 <shaan.hurley25@gmail.com>
2020-10-29 15:41:26 -0700

    added test for isFizzy and isBuzzy

--- tests/test.js
+++ tests/test.js
@@ -3,14 +3,21 @@ const { expect } = require("@jest/globals")
 
 const fb = require("../fizzbuzz")
 
+test("Test isFizzy", () => {
+    expect(fb.isFizzy(3)).toBe(true)
+    expect(fb.isFizzy(1)).toBe(false)
+    expect(fb.isFizzy(9)).toBe(true)
+    expect(fb.isFizzy(102)).toBe(true)
+    expect(fb.isFizzy(2)).toBe(false)
+})
 
 
+test("Test isBuzzy", () => {
+    expect(fb.isBuzzy(5)).toBe(true)
+    expect(fb.isBuzzy(100)).toBe(true)
+    expect(fb.isBuzzy(12)).toBe(false)
+    expect(fb.isBuzzy(6)).toBe(false)
+    expect(fb.isBuzzy(105)).toBe(true)
+    expect(fb.isBuzzy(1)).toBe(false)
+})
 
-// test('Sanity check', () => {
-//     expect(2+2).toBe(4)
-//   })
-
-
-//   test('Testing isFizzy', () => {
-//       expect(isFizzy(3)).toBe(true)
-//   })
