00eac16dce340d7f47b04100200e772be02fe85d
shaannessy25 <shaan.hurley25@gmail.com>
2020-10-29 15:20:59 -0700

    initial commit

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const { expect } = require("@jest/globals")
+
+
+const fb = require("../fizzbuzz")
+
+
+
+
+// test('Sanity check', () => {
+//     expect(2+2).toBe(4)
+//   })
+
+
+//   test('Testing isFizzy', () => {
+//       expect(isFizzy(3)).toBe(true)
+//   })
